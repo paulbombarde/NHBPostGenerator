17ccc315d6b78ae4d03cd9ab906b1a10d69bbb07
Paul Bombarde <pbombarde@logitech.com>
2026-01-05 22:37:37 +0100

    Jan 2026 update

--- generator.py
+++ generator.py
@@ -55,20 +55,89 @@ teams_replacements = {
     "SG Genève Paquis - Lancy PLO": "Genève Paquis - Lancy",
     "SG Genève /TCGG/ Nyon": "SG Genève/TCGG/Nyon",
     "SG Wacker Thun 2 / Steffisburg": "Wacker Thun/Steffisburg",
-    "SG Troinex / Chênois Genève": "SG Troinex/Chênois"
+    "SG Troinex / Chênois Genève": "SG Troinex/Chênois",
+    "CS Chênois Genève Handball": "Chênois Genève HB",
+    "RG Chênois Genève / Troinex": "Chênois Genève / Troinex",
+    "- RG Chênois Genève / Troinex": "Chênois Genève / Troinex",
+    "HBC Etoy 1 -": "HBC Etoy",
+    "RG Rive Gauche Handball": "RG Rive Gauche HB",
+    "HBC Vallée de Joux ° -": "HBC Vallée de Joux",
+    "HBC Moudon -": "HBC Moudon"
 }
 
 level_replacements = {
     "M15G-P S1-06": "M15P",
     "M13G-P S1-06": "M13P",
+    "M13G-P S1-08": "M13P-S1",
+    'MU13P S1-07': 'M13P-S1',
+    'M13G-P S1-07': 'M13P-S1',
+    "MU13P S2-14": "M13P-S2",
+    "M13G-P S2-14": "M13P-S2",
+    'MU17P S1-07': "M17P",
+    'M17G-P S1-07': "M17P",
     "H1-03": "1ière Ligue Hommes",
+    'M2-06': "Ligue 2 Hommes",
+    'H2-06': "Ligue 2 Hommes",
     "H4-09": "H4",
     "M14F-P-06": "M14P",
+    'M14F-P-02 Prm': "M14P",
+    'M14F-I Rlg-02': "M14I",
+    'FU14I-03': "M14I",
+    'M14F-I-03': "M14I",
     "M16F-P-08": "M16P",
+    "M16F-P-06": "M16P",
+    'FU16I-03': "M16I",
+    'M16F-I-03': "M16I",
+    "M16F-P-02 Prm" : "M16P",
     "D3-08": "3ième Ligue Dames",
+    "F3-08": "Ligue 3 Dames",
     "Cup Mobilière H - Tour de qualification": "Cup Mobilière H",
     "Cup R H": "Cup R H - 1/8",
-    'Cup R M15 G':'Cup R M15 G'
+    "Cup R D": "Cup R D",
+    'Cup R M15 G':'Cup R M15 G',
+    'R-Cup M':'R-Cup M',
+    'M15G-P S1-05 Prm': 'M15 Barrages',
+    'M15G-I Rlg-02': "M15I",
+    'M14F-P-08': "M14P",
+    'M17G-P S1-08': "M17P",
+    'M15G-P S2-12': "M15P",
+    'M14F E/I-01': "M14E/I",
+    'M13G-P S2-12': "M13P",
+    'Cup R M16 F': "R Cup",
+}
+
+teams_logos={
+    "NHB La Côte": "NHB.png",
+    "LVC Handball": "LVC.png",
+    "LVC Handball 2": "LVC.png",
+    "KTV Visp Handball": "Visp.png",
+    'Handball Oberaargau': "HVH.png",
+    "SG TV Solothurn": "TV_Solothurn.png",
+    "SG WEST Crissier": "crissier.png",
+    "SG TV Birsfelden": "TVBirsfelden.png",
+    'TV Pratteln NS 1': "NSPratteln.png",
+    "Wacker Thun/Steffisburg": "Wacker_Thun.png",
+    "HBC Neuchâtel": "Neuchatel.png",
+    "HBC Moudon": "HBCMoudon.png",
+    "PSG Lyss F3": "PSGLyss.png",
+    "SG Vaud La Côte 2": "HBC - Nyon.png",
+    "HBC Etoy": "etoy.png",
+    "SG Möhlin/Magden": "Mohlin.png",
+    "SG Nyon": "HBC - Nyon.png",
+    "HS Biel Bienne": "HS Bienne.png",
+    'RG Vaud La Côte 1': "tcgg.png",
+    'RG Crissier-West M23': "crissier.png",
+    'SG Bern City': "sgberncity.png",
+    'HBC La Chaux-de-Fonds': "hbc-cdf.png",
+    'SG US Yverdon / RSB': 'usyverdon.png',
+    'RG Rive Gauche Handball': 'Rive_gauche1.png',
+    'RG Rive Gauche HB': 'Rive_gauche1.png',
+    'RG Vaud La Côte 2': "tcgg.png",
+    'HG Bödeli': "hg-bodeli.png",
+    'BSV Bern 3': "BSVBern.png",
+    'HC Servette 1': 'Servette_hbc_logo.png',
+    'SG TV Steffisburg 2 / Wacker':  "tvsteffisburg.png",
+    'HC Vevey 1': "vevey.png",
 }
 
 players = {
@@ -127,7 +196,7 @@ def replace_all(tree, replacements):
         if not replacements:
             return
 
-def update_template(template_name, date, _replacements, png=True):
+def update_template(template_name, date, _replacements, png=True, write=True):
     replacements = deepcopy(_replacements)
 
     output_name = date+'_'+template_name
@@ -138,10 +207,11 @@ def update_template(template_name, date, _replacements, png=True):
     svg_root = svg.getroot()
     replace_all(svg_root, replacements)
     replace_logos(svg_root, _replacements)
-    svg.write(svg_output)
 
-    if png:
-        convert_to_png(svg_output)
+    if write:
+        svg.write(svg_output)
+        if png:
+            convert_to_png(svg_output)
 
     return svg_output
 
@@ -191,6 +261,9 @@ def parse_calendar(ics_file_path):
 
     dates_to_matches = defaultdict(list)
     for event in calendar.events:
+        if 'Nyon Handball - La Côte' in event.name:
+            event.name = event.name.replace('Nyon Handball - La Côte', 'Nyon HandBall La Côte')
+
         if not 'Nyon HandBall La Côte' in event.name:
             continue
 
@@ -213,7 +286,7 @@ def replacements_from_hd_match(match, id):
 def replacements_from_match(match, id):
     t1 = match.team1
     t2 = match.team2
-    if t1.startswith("NHB"):
+    if t1.startswith(team_text_marker):
         t1 += "-"+normalize_level(match.level)
     else:
         t2 += "-" + normalize_level(match.level)
@@ -234,49 +307,55 @@ def generate_posts(dates_to_matches, start= date.today()):
 
         other_matches = []
         for match in matches:
-            rs = {"date": convert_date(date)}
-            if match.level.startswith("H1"):
-                rs |= replacements_from_hd_match(match,1)
-                update_template('story_match_day', date.isoformat(), rs)
-                update_template('match_day_h1', date.isoformat(), rs)
-                update_template('results_h1', date.isoformat(), rs, False)
-                update_template('story_players', date.isoformat(), rs, False)
-            elif match.level.startswith("D3"):
-                rs |= replacements_from_hd_match(match,1)
-                update_template('story_match_day_dames', date.isoformat(), rs)
-                update_template('match_day_d3', date.isoformat(), rs)
-                update_template('results_d3', date.isoformat(), rs, False)
+            if match.level.startswith("M2") or match.level.startswith("H2"):
+                generate_post_h1(match, date)
+            elif match.level.startswith("F3") or match.level.startswith("D3"):
+                generate_post_d3(match, date)
             else:
                 other_matches.append(match)
 
-        if other_matches:
-            rs = {"date": convert_date(date)}
-            md_template = "match_day_"+str(len(other_matches))
-            r_template = "results_"+str(len(other_matches))
-            other_matches.sort(key=lambda m: m.time)
-            for i, match in enumerate(other_matches, start=1):
-                rs |= replacements_from_match(match, i)
-            update_template(md_template, date.isoformat(), rs)
-            update_template(r_template, date.isoformat(), rs, False)
+        if 4 < len(other_matches):
+            # more than 4 matches on the same image would be ugly
+            # We try first to separate at home games for external ones
+            home_games = [g for g in other_matches if team_text_marker in g.team1]
+            ext_games = [g for g in other_matches if team_text_marker in g.team2]
+            if len(home_games) < 5 and len(ext_games) < 5:
+                # Ok, let's generate them now
+                generate_other_posts(home_games, date, "_home_")
+                generate_other_posts(ext_games, date, "_ext_")
+            else:
+                matches.sort(key=lambda m: m.time)
+                generate_other_posts(matches[:len(matches)//2], date, "_1_")
+                generate_other_posts(matches[len(matches)//2:], date, "_2_")
+        else:
+            generate_other_posts(other_matches, date)
 
+def generate_post_h1(match, date):
+    rs = {"date": convert_date(date)}
+    rs |= replacements_from_hd_match(match, 1)
+    update_template('story_match_day', date.isoformat(), rs)
+    update_template('match_day_h1', date.isoformat(), rs)
+    update_template('results_h1', date.isoformat(), rs, False)
+    update_template('story_players', date.isoformat(), rs, False)
 
-teams_logos={
-    "NHB La Côte": "NHB.png",
-    "LVC Handball": "LVC.png",
-    "LVC Handball 2": "LVC.png",
-    "KTV Visp Handball": "Visp.png",
-    'Handball Oberaargau': "HVH.png",
-    "SG TV Solothurn": "TV_Solothurn.png",
-    "SG WEST Crissier": "crissier.png",
-    "SG TV Birsfelden": "TVBirsfelden.png",
-    'TV Pratteln NS 1': "NSPratteln.png",
-    "Wacker Thun/Steffisburg": "Wacker_Thun.png",
-    "HBC Neuchâtel": "Neuchatel.png",
-    "HBC Moudon": "HBCMoudon.png",
-    "PSG Lyss F3": "PSGLyss.png",
-    "SG Vaud La Côte 2": "HBC - Nyon.png",
-    "HBC Etoy": "etoy.png",
-}
+def generate_post_d3(match, date):
+    rs = {"date": convert_date(date)}
+    rs |= replacements_from_hd_match(match, 1)
+    update_template('story_match_day_dames', date.isoformat(), rs)
+    update_template('match_day_d3', date.isoformat(), rs)
+    update_template('results_d3', date.isoformat(), rs, False)
+
+def generate_other_posts(matches, date, extra_name=""):
+    if not matches:
+        return
+    rs = {"date": convert_date(date)}
+    md_template = "match_day_" + str(len(matches))
+    r_template = "results_" + str(len(matches))
+    matches.sort(key=lambda m: m.time)
+    for i, match in enumerate(matches, start=1):
+        rs |= replacements_from_match(match, i)
+    update_template(md_template, date.isoformat()+extra_name, rs)
+    update_template(r_template, date.isoformat()+extra_name, rs, False)
 
 def replace_logos(svg_tree, _replacements):
     for logo_label, team_label in [("match1-logo-team1", "match1-team1"), ("match1-logo-team2", "match1-team2")]:
@@ -319,8 +398,8 @@ def find_image(svg, image_label):
 
 
 if __name__ == "__main__":
-    os.makedirs(os.path.dirname(svg_output_folder), exist_ok=True)
-    os.makedirs(os.path.dirname(png_output_folder), exist_ok=True)
+    os.makedirs(svg_output_folder, exist_ok=True)
+    os.makedirs(png_output_folder, exist_ok=True)
 
     matches = parse_calendar(sys.argv[1])
     generate_posts(matches)
